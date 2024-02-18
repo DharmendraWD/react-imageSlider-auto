92224624a02c595816e67093c61183152356e60c
Dharmendra Sharma <115578231+DharmendraWD@users.noreply.github.com>
2024-02-18 21:12:19 +0545

    Add files via upload

---
+++ src/components/Slideshow.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./Slideshow.css";
+import image1 from "./images/img1.jpg"
+import image2 from "./images/img2.jpg"
+import image3 from "./images/img3.jpg"
+import image4 from "./images/img4.jpg"
+import image5 from "./images/img5.jpg"
+
+const images = [
+  {image: image1 },
+  {image: image2 },
+  {image: image3 },
+  {image: image4 },
+  {image:image5 }]
+const delay = 3000;
+
+const Slideshow = () => {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {images.map((image, index) => (
+          <img className="slide" src={image.image} alt="bird image"/>
+        ))}
+      </div>
+      <div className="slideshowDots">
+        {images.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;
